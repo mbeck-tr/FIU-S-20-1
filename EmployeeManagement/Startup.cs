46063b4973c460fca143adcd50d5ad1f15b51fbd
Michael Beck <mbeck@lutzundgrub.de>
2021-02-01 11:22:00 +0100

    Middleware hinzugefügt, Pipelineablauf geloggt

---
+++ EmployeeManagement/Startup.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> logger)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Use(async (context, next) =>
+            {
+                logger.LogInformation("MW1: Incoming Request");
+                await next();
+                logger.LogInformation("MW1: Outgoing response");
+            });
+
+            app.Use(async (context, next) =>
+            {
+                logger.LogInformation("MW2: Incoming Request");
+                await next();
+                logger.LogInformation("MW2: Outgoing response");
+            });
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("MW3: Hello from 3. Middleware");
+                logger.LogInformation("MW3: Request handled and response produced");
+                await next();
+                logger.LogInformation("MW3: Outgoing response");
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("MW4: Hello from 4. Middleware");
+                logger.LogInformation("MW4: Request handled and response produced");
+            });
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # FIU-S-20-1
+
+## Voraussetzungen
+
+- https://dotnet.microsoft.com/download
+- .NET core SDK
+- .NET core Runtime
